bb84d1d55884658c3af3de2353f5b9b0e2b8f528
Tony <un_razum@mail.ru>
2021-12-09 21:54:06 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+let firstPlayer = {
+    name: "Федор",
+    hp: 100,
+    img: "//",
+    weapon: [gun, bow],
+    attack: function() { console.log(this.name + " fight " + "Fight..."); },
+}
+
+let secondPlayer = {
+    name: "Федорка",
+    hp: 100,
+    img: "//",
+    weapon: [gun, bow],
+    attack: function() { console.log(this.name + " fight " + "Fight..."); },
+}
+
+function creatPlayer(player, name, health = 100) {
+    arena.appendChild()
+}
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
